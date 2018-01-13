b41f99869d16bc3c86f57886607968c8ffa03d0f
onluiz <luiz.alberto1723@gmail.com>
2018-01-13 10:54:35 -0200

    update

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
-var cool = require('cool-ascii-faces')
 const express = require('express')
+const app = express();
 const path = require('path')
+const ejs = require('ejs')
 const PORT = process.env.PORT || 5000
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .get('/cool', (req, res) => {
-    res.send(cool());
-  })
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+// app.use(express.static(path.join(__dirname, 'public')))
+// app.set('views', __dirname + '/views');
+// app.engine('html', ejs.renderFile);
+
+app.engine('html', require('ejs').renderFile);
+
+app.get('/', function(req,res) {
+  res.sendFile(__dirname + '/views/pages/index.html');
+ })
+
+//last line
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+// express()
+  
+//   .set('views', path.join(__dirname, 'views'))
+//   .set('view engine', 'html')
+//   .get('/', (req, res) => res.render('pages/index.html'))
+//   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
