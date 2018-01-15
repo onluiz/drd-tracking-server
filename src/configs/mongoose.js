511e1238d9a931aca0d91a73faf9e4a96a29d259
onluiz <luiz.alberto1723@gmail.com>
2018-01-15 01:10:01 -0200

    lala

--- src/configs/mongoose.js
+++ src/configs/mongoose.js
@@ -1,9 +1,7 @@
 module.exports = () => {
   var mongoose = require('mongoose');
   mongoose.Promise = global.Promise;
-  // mongoose.connect('mongodb://localhost/drdtrackingdb4', {useMongoClient: true});
-  mongoose.connect('mongodb://rd:17231723@ds157057.mlab.com:57057/heroku_2z8xvtrx', {useMongoClient: true});
-  //mongodb://heroku_2z8xvtrx:itachi312@ds157057.mlab.com:57057/heroku_2z8xvtrx
+  mongoose.connect('mongodb://localhost/drdtrackingdb', {useMongoClient: true});
 
   var db = mongoose.connection;
   db.on('error', console.error.bind(console, 'connection error:'));
